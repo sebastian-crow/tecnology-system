307532eace2c3191e0f0f0a07fd8c49eb63ae2fb
Camilo Burbano <camiloburbano82@gmail.com>
2023-02-07 09:42:51 -0500

    ajustes 4

--- main.js
+++ main.js
@@ -56,6 +56,7 @@ createApp({
     adolecente < 17
     adulto >18 && <60
     anciano < 60
+
     valida si el año ingresado por el usuario es el año generado aleatoriamente
     si es asi se llamara la funcion anyoNacimiento()
     de lo contrario se llamara la funcion darPista()
@@ -159,19 +160,6 @@ createApp({
             }
           )
 
-          console.log(this.intentos.length )
-          if(this.intentos.length > 4){
-            this.jugadores.push(
-              {
-                'nombre': this.nombre,
-                'intentos': this.intentos
-              }
-            )
-            this.mensaje =" juego terminado"
-            console.log(this.mensaje)
-            localStorage.setItem('registro', JSON.stringify(this.jugadores))
-            this.intentos =[]
-          }
           //seteamos el intento
           // this.intento = 0;
 
@@ -240,8 +228,5 @@ createApp({
     }
 
   },
-}).mount("#root")
-
-
-
+}).mount("#root");
 
